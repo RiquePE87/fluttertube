da58e4415784c63dc966ab6303a8a99e11d2e8b5
RiquePE87 <rickpe81@gmail.com>
2020-05-17 22:36:55 -0300

    1705

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertube/blocs/videos_bloc.dart';
 import 'package:fluttertube/screens/home_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'FlutterTube',
-    home: HomeScreen(),
-  ));
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      blocs: [Bloc((i) => VideosBloc())],
+      child:MaterialApp(
+        home: HomeScreen(),
+        title: 'FlutterTube',
+      ),
+
+    );
+  }
+}
+
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,14 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertube/blocs/videos_bloc.dart';
 import 'package:fluttertube/delegates/data_search.dart';
+import 'package:fluttertube/widgets/video_tile.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black87,
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black87,
         elevation: 0,
@@ -21,13 +24,31 @@ class HomeScreen extends StatelessWidget {
           ),
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: (){
-              showSearch(context: context, delegate: DataSearch());
+            onPressed: () async{
+              String result = await showSearch(context: context, delegate: DataSearch());
+              if (result != null){
+                BlocProvider.getBloc<VideosBloc>().inSearch.add(result);
+              }
             },
           )
         ],
       ),
-      body: Container(),
+      body: StreamBuilder(
+        builder: (context, snapshot){
+          if (snapshot.hasData){
+            return ListView.builder(
+                itemBuilder: (context,index){
+                  return VideoTile(snapshot.data[index]);
+                },
+              itemCount: 10,
+            );
+          }else{
+            return Container();
+          }
+
+        },
+        stream: BlocProvider.getBloc<VideosBloc>().outVideos,
+      ),
     );
   }
 }
